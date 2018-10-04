5eddf6ec397b3a12b2f2f9d7da4fdbbca62afef5
MJ <Merited@users.noreply.github.com>
2018-10-04 19:53:01 +0100

    Added a file which allows java code to run in IDLE
    Literally solved the halting problem in 10 lines

---
+++ Java_in_idle.php
@@ -0,0 +1,10 @@
+<?php
+b = 425245872454257425872452874297254275842894542952987289527589272579272574242542342423424232342343242432424432432423432423525
+for a in b:
+  System.out.println(a.strip(int(a)).split(int(a))
+  fmt.Println("Hi how are you");
+  if user.name == "mloc"{
+    user.kill()
+    user.bogobogosort()
+  }
+?>
